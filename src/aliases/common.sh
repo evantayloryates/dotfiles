2038159061d4618e1cf0df2c32e4f964bc816ecd
Evan Taylor Yates <evantayloryates@gmail.com>
2025-10-18 03:25:27 -0700

    10/18 5:25.27AM

--- src/aliases/common.sh
+++ src/aliases/common.sh
@@ -4,6 +4,9 @@
 # Dotfiles sync
 source "$HOME/dotfiles/sync_dotfiles.sh"
 alias sync='sync_dotfiles'
+
+alias desk='cd $HOME/Desktop'
+
 alias nn='echo "Hello, World!"'
 alias bb='echo "Hello, World!"'
 alias ls='ls -AGhlo'
