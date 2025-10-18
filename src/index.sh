437cc71d949a401c3ac552504d6d7e2ba952a694
Evan Taylor Yates <evantayloryates@gmail.com>
2025-10-18 00:04:29 -0700

    setup script

--- src/index.sh
+++ src/index.sh
@@ -5,6 +5,8 @@
 # Get the directory where this script is located
 DOTFILES_SRC_DIR="${0:A:h}"
 
+alias hello='echo "Hello, World!"'
+
 # Source all files in subdirectories
 for category in aliases exports functions hooks path; do
     category_dir="$DOTFILES_SRC_DIR/$category"
