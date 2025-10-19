6485ba023edff0e4e1a1093acb3d58d23bd0a55b
Evan Taylor Yates <evantayloryates@gmail.com>
2025-10-18 22:08:13 -0400

    10/18 9:08.13PM

--- src/functions/common.sh
+++ src/functions/common.sh
@@ -1,1 +1,13 @@
 #!/bin/zsh
+
+
+CONFIG='[
+  {
+    "slug": "amp",
+    "path": "/Users/taylor/src/github/amplify",
+    "default": "cd",
+    "commands": {
+      "ls": "ls -AGhlo <path> | grep"
+    }
+  }
+]'
