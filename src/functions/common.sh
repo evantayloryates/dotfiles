487117936e8d2b188331f5d3c7d390ca1345aeef
Evan Taylor Yates <evantayloryates@gmail.com>
2025-10-18 22:16:44 -0400

    10/18 9:16.44PM

--- src/functions/common.sh
+++ src/functions/common.sh
@@ -1,46 +1,11 @@
 #!/bin/zsh
 
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-CONFIG='[
-  {
-    "slug": "amp",
-    "path": "/Users/taylor/src/github/amplify",
-    "default": "cd",
-    "commands": {
-      "ls": "ls -AGhlo <path> | grep"
-    }
-  }
-]'
-
-build_path_functions() {
-  local config_json="$1"
-  local count
-  count=$(echo "$config_json" | jq 'length')
-
-  for i in $(seq 0 $((count - 1))); do
-    local slug path default_command
-    slug=$(echo "$config_json" | jq -r ".[$i].slug")
-    path=$(echo "$config_json" | jq -r ".[$i].path")
-    default_command=$(echo "$config_json" | jq -r ".[$i].default")
-
-    eval "
-${slug}() {
-  local subcmd=\"\$1\"
-  shift || true
-
-  case \"\$subcmd\" in
-$(echo "$config_json" | jq -r ".[$i].commands | to_entries[] | \"    \(.key))\\n      \(.value | gsub(\"<path>\"; \"$path\")) \\\"\\\$@\\\"\\n      ;;\"")
-    *)
-      if [[ -z \"\$subcmd\" ]]; then
-        $default_command \"$path\"
-      else
-        $subcmd \"$path\" \"\$@\"
-      fi
-      ;;
-  esac
-}
-"
-  done
-}
-
-build_path_functions "$CONFIG"
+# Generate the function file and source it
+PATHFUNCS_FILE="$(python3 $SCRIPT_DIR/build_pathfuncs.py)"
+if [[ -f "$PATHFUNCS_FILE" ]]; then
+  source "$PATHFUNCS_FILE"
+else
+  echo "Failed to generate path functions"
+fi
