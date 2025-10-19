db20bc17aba9f3ecd3056707fbe051a441a903c5
Evan Taylor Yates <evantayloryates@gmail.com>
2025-10-18 21:32:40 -0400

    10/18 8:32.40PM

--- src/hooks/prompt_customizations.sh
+++ src/hooks/prompt_customizations.sh
@@ -17,15 +17,18 @@ function pretty_date() {
 
   local am_pm_char=$(echo "$current_time" | awk '{print substr($0, length($0))}')
 
-  if [[ ${#current_time} -eq 7 ]]; then
-    current_time=" $current_time"
-  fi
-
   local prefix_time=$(echo "$current_time" | awk '{print substr($0, 1, index($0, ":") - 1)}')  
   local suffix_time=$(echo "$current_time" | awk '{print substr($0, index($0, ":") + 1, length($0) - index($0, ":"))}')
   suffix_time=$(echo "$suffix_time" | awk '{print substr($0, 1, length($0)-1)}')
 
-  local colored_prefix=$(red "$prefix_time")
+  # Prefix single-digit hours with dark gray 0
+  local colored_prefix
+  if [[ ${#prefix_time} -eq 1 ]]; then
+    colored_prefix="$(darkgray "0")$(red "$prefix_time")"
+  else
+    colored_prefix=$(red "$prefix_time")
+  fi
+  
   local colored_suffix=$(red "$suffix_time")
 
   current_time="${colored_prefix}:%B%F{white}%b%f${colored_suffix}%F{white}${am_pm_char}%f"
