5e37decf19d350a5451ce5eae484dcd593b264eb
Evan Taylor Yates <evantayloryates@gmail.com>
2025-10-18 23:28:35 -0400

    10/18 10:28.35PM

--- src/functions/common.sh
+++ src/functions/common.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Generate the function file and source it
-PATHFUNCS_FILE="$(python3 $SCRIPT_DIR/build_pathfuncs.py)"
+PATHFUNCS_FILE="$(python3 $DOTFILES_DIR/src/python/pathfuncs.py)"
 if [[ -f "$PATHFUNCS_FILE" ]]; then
   source "$PATHFUNCS_FILE"
 else
