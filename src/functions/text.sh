94669db77d80cb2eea41dc30f7f21053bff8d07b
Evan Taylor Yates <evantayloryates@gmail.com>
2025-10-19 01:25:24 -0400

    10/19 12:25.24AM

--- src/functions/text.sh
+++ src/functions/text.sh
@@ -39,3 +39,19 @@ quick() {
   pbcopy < "$output_path"
   echo "📋 Copied response to clipboard"
 }
+
+
+research() {
+  prompt="$*"
+  filepath=$(python3 "$DOTFILES_DIR/src/python/replicate.research.py" "$prompt")
+  if [[ -z "$filepath" || ! -f "$filepath" ]]; then
+    echo "Failed to generate text"
+    return 1
+  fi
+  printf '\n\n'
+  cat "$filepath"
+  printf '\n\n'
+
+  pbcopy < "$filepath"
+  echo "📋 Copied response to clipboard"
+}
