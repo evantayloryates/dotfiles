e13fb5ea9683e91a11c3f9e6b461bb478c8ef782
Evan Taylor Yates <evantayloryates@gmail.com>
2025-10-20 11:26:40 -0400

    10/20 10:26.40AM

--- src/functions/common.sh
+++ src/functions/common.sh
@@ -16,3 +16,36 @@ for f in "$SCRIPT_DIR"/*.sh; do
 done
 
 
+function launch ()
+{
+	# Initialize dirs in case they don't exist
+	mkdir /Library/LaunchDaemons 2>/dev/null
+	mkdir ~/Library/LaunchDaemons 2>/dev/null
+	mkdir /Library/LaunchAgents 2>/dev/null
+	mkdir ~/Library/LaunchAgents 2>/dev/null
+
+	# System and User Daemons
+	if [ "$1" = "d" ]; then
+		print_daemons
+	
+	# System and User Agents
+	elif [ "$1" = "a" ]; then
+		print_agents
+
+	# System and User Daemons and Agents
+	elif [ "$1" = "" ]; then
+		print_agents
+		print_daemons
+	
+	# Invalid arg
+	else
+		echo "
+Invalid argument: \`$1\`
+
+Valid commands:
+	\`launch d\` => Open launch daemon directories
+	\`launch a\` => Open launch agent directories
+	\`launch\`   => Open launch agent and daemon directories
+"
+	fi
+}
