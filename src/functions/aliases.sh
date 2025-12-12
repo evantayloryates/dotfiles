def9224769937e2480f1699249803a1a01fe7e6a
Evan Taylor Yates <evantayloryates@gmail.com>
2025-12-12 03:45:35 -0500

    12/12 3:45.35AM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -3,17 +3,17 @@
 # This file must follow the pattern always. no divergences
 # A "Note" is anything that will provide context for the pattern.
 # : is offical syntax. 
-_ ' Note: this will overwrite the /usr/bin/ex command'; ex() { exiftool "$@"; }
-_; abs     () { realpath "$@"                                       ;}
-_; c       () { cursor "$@"                                         ;}
-_; clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy    ;}
-_; convert () { magick "$@"                                         ;}
-_; env     () { /usr/bin/env | sort                                 ;}
-_; ls      () { /bin/ls -AGhlo "$@"                                 ;}
-_; path    () { python3 "$DOTFILES_DIR/src/python/path.py"          ;}
-_; pip3    () { pip "$@"                                            ;}
-_; python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"    ;}
-_; python3 () { python "$@"                                         ;}
-_; src     () { exec "$SHELL" -l                                    ;}
+_; ex() { exiftool "$@"                                             ;} # Note: this will overwrite the /usr/bin/ex command
+_; abs     () { realpath "$@"                                       ;} # 
+_; c       () { cursor "$@"                                         ;} # 
+_; clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy    ;} # 
+_; convert () { magick "$@"                                         ;} # 
+_; env     () { /usr/bin/env | sort                                 ;} # 
+_; ls      () { /bin/ls -AGhlo "$@"                                 ;} # 
+_; path    () { python3 "$DOTFILES_DIR/src/python/path.py"          ;} # 
+_; pip3    () { pip "$@"                                            ;} # 
+_; python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"    ;} # 
+_; python3 () { python "$@"                                         ;} # 
+_; src     () { exec "$SHELL" -l                                    ;} # 
 
 
