2122e84702072e17d683ff01d5ecc976112238f2
Evan Taylor Yates <evantayloryates@gmail.com>
2025-12-12 02:06:53 -0500

    12/12 2:06.53AM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -3,16 +3,11 @@
 # A "Note" is anything that will provide context for the pattern.
 # : is offical syntax. 
 
-# Note: 
-abs() { realpath "$@"; }
-# Note: —
-c() { cursor "$@"; }
-# Note: —
-convert() { magick "$@"; }
-# Note: —
-
+_; abs() { realpath "$@"; }
+_; c() { cursor "$@"; }
+_; convert() { magick "$@"; }
 _; env() { /usr/bin/env | sort; }
-ex() { exiftool "$@"; }
-ls() { /bin/ls -AGhlo "$@"; }
-path() { python3 "$DOTFILES_DIR/src/python/path.py"; }
-src() { exec "$SHELL" -l; }
+_; ex() { exiftool "$@"; }
+_; ls() { /bin/ls -AGhlo "$@"; }
+_; path() { python3 "$DOTFILES_DIR/src/python/path.py"; }
+_; src() { exec "$SHELL" -l; }
