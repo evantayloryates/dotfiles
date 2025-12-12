fa2f92a5e14f4a0c7db3f578ec36c59579ef48d1
Evan Taylor Yates <evantayloryates@gmail.com>
2025-12-12 03:46:27 -0500

    12/12 3:46.27AM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -3,17 +3,17 @@
 # This file must follow the pattern always. no divergences
 # A "Note" is anything that will provide context for the pattern.
 # : is offical syntax. 
-abs     () { realpath "$@"                                       ;} # 
-c       () { cursor "$@"                                         ;} # 
-clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy    ;} # 
-convert () { magick "$@"                                         ;} # 
-env     () { /usr/bin/env | sort                                 ;} # 
-ex      () { exiftool "$@"                                       ;} # Note: this will overwrite the /usr/bin/ex command
-ls      () { /bin/ls -AGhlo "$@"                                 ;} # 
-path    () { python3 "$DOTFILES_DIR/src/python/path.py"          ;} # 
-pip3    () { pip "$@"                                            ;} # 
-python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"    ;} # 
-python3 () { python "$@"                                         ;} # 
-src     () { exec "$SHELL" -l                                    ;} # 
+abs     () { realpath "$@"                                                ;} # 
+c       () { cursor "$@"                                                  ;} # 
+clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy             ;} # 
+convert () { magick "$@"                                                  ;} # 
+env     () { /usr/bin/env | sort                                          ;} # 
+ex      () { exiftool "$@"                                                ;} # Note: this will overwrite the /usr/bin/ex command
+ls      () { /bin/ls -AGhlo "$@"                                          ;} # 
+path    () { python3 "$DOTFILES_DIR/src/python/path.py"                   ;} # 
+pip3    () { pip "$@"                                                     ;} # 
+python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"             ;} # 
+python3 () { python "$@"                                                  ;} # 
+src     () { exec "$SHELL" -l                                             ;} # 
 
 
