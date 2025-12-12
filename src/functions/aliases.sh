65882e4ce07c465dd308748d54060104f8b116ee
Evan Taylor Yates <evantayloryates@gmail.com>
2025-12-12 03:44:27 -0500

    12/12 3:44.27AM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -4,16 +4,16 @@
 # A "Note" is anything that will provide context for the pattern.
 # : is offical syntax. 
 _ ' Note: this will overwrite the /usr/bin/ex command'; ex() { exiftool "$@"; }
-_; abs() { realpath "$@"; }
-_; c() { cursor "$@"; }
-_; clip() { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy; }
-_; convert() { magick "$@"; }
-_; env() { /usr/bin/env | sort; }
-_; ls() { /bin/ls -AGhlo "$@"; }
-_; path() { python3 "$DOTFILES_DIR/src/python/path.py"; }
-_; pip3() { pip "$@"; }
-_; python() { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"; }
-_; python3() { python "$@"; }
-_; src() { exec "$SHELL" -l; }
+_; abs     () { realpath "$@"                                       ;}
+_; c       () { cursor "$@"                                         ;}
+_; clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy    ;}
+_; convert () { magick "$@"                                         ;}
+_; env     () { /usr/bin/env | sort                                 ;}
+_; ls      () { /bin/ls -AGhlo "$@"                                 ;}
+_; path    () { python3 "$DOTFILES_DIR/src/python/path.py"          ;}
+_; pip3    () { pip "$@"                                            ;}
+_; python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"    ;}
+_; python3 () { python "$@"                                         ;}
+_; src     () { exec "$SHELL" -l                                    ;}
 
 
