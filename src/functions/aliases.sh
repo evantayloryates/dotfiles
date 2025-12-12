32d58ce1a6f014de842aa3ea321edbfa27226a4c
Evan Taylor Yates <evantayloryates@gmail.com>
2025-12-12 03:46:02 -0500

    12/12 3:46.02AM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -3,17 +3,17 @@
 # This file must follow the pattern always. no divergences
 # A "Note" is anything that will provide context for the pattern.
 # : is offical syntax. 
-_; ex() { exiftool "$@"                                             ;} # Note: this will overwrite the /usr/bin/ex command
-_; abs     () { realpath "$@"                                       ;} # 
-_; c       () { cursor "$@"                                         ;} # 
-_; clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy    ;} # 
-_; convert () { magick "$@"                                         ;} # 
-_; env     () { /usr/bin/env | sort                                 ;} # 
-_; ls      () { /bin/ls -AGhlo "$@"                                 ;} # 
-_; path    () { python3 "$DOTFILES_DIR/src/python/path.py"          ;} # 
-_; pip3    () { pip "$@"                                            ;} # 
-_; python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"    ;} # 
-_; python3 () { python "$@"                                         ;} # 
-_; src     () { exec "$SHELL" -l                                    ;} # 
+ex      () { exiftool "$@"                                       ;} # Note: this will overwrite the /usr/bin/ex command
+abs     () { realpath "$@"                                       ;} # 
+c       () { cursor "$@"                                         ;} # 
+clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy    ;} # 
+convert () { magick "$@"                                         ;} # 
+env     () { /usr/bin/env | sort                                 ;} # 
+ls      () { /bin/ls -AGhlo "$@"                                 ;} # 
+path    () { python3 "$DOTFILES_DIR/src/python/path.py"          ;} # 
+pip3    () { pip "$@"                                            ;} # 
+python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"    ;} # 
+python3 () { python "$@"                                         ;} # 
+src     () { exec "$SHELL" -l                                    ;} # 
 
 
