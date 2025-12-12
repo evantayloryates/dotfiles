d138a98c71093199055616306ba31a8f6a65993c
Evan Taylor Yates <evantayloryates@gmail.com>
2025-12-12 01:52:52 -0500

    12/12 1:52.52AM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -1,4 +1,19 @@
-c() {
-  cursor "$@"
-}
+# Aliases here always mean functions
 
+
+# Note: none
+abs() { realpath "$@"; }
+# Note: none
+c() { cursor "$@"; }
+# Note: none
+convert() { magick "$@"; }
+# Note: none
+env() { /usr/bin/env | sort; }
+# Note: this will overwrite the /usr/bin/ex command
+ex() { exiftool "$@"; }
+# Note: none
+ls() { /bin/ls -AGhlo "$@"; }
+# Note: none
+path() { python3 "$DOTFILES_DIR/src/python/path.py"; }
+# Note: none
+src() { exec "$SHELL" -l; }
