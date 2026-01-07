259f85e0f415760991e7f1f59ebb6948911d47da
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-07 18:47:19 -0500

    01/07 6:47.19PM

--- src/functions/copiers.sh
+++ src/functions/copiers.sh
@@ -4,7 +4,12 @@
 
 __ () {
   # Lists all copier functions in this file
-  declare -F | awk '{print $3}' | grep '^_' | grep -v '^__$'
+  # List copier functions (starting with _), excluding __, and log function and value sorted alphabetically
+  for fn in $(declare -F | awk '{print $3}' | grep '^_' | grep -v '^__$' | sort); do
+    # get string to be copied by capturing the command before /usr/bin/pbcopy
+    body="$(declare -f "$fn" | grep -v '^}' | grep 'pbcopy' | sed -nE "s/.*printf[[:space:]]+['\"]([^'\"]*)['\"].*/\1/p")"
+    printf "%s:\n\n%s\n\n" "$fn" "${body:-<could not determine value>}"
+  done
 }
 
 _rs () { printf "Hill world" | /usr/bin/pbcopy ;}
