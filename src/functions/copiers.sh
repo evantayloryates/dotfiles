1fa7809dc5c5eaba41be94a13e45ef4e35041899
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-07 18:47:48 -0500

    01/07 6:47.48PM

--- src/functions/copiers.sh
+++ src/functions/copiers.sh
@@ -2,14 +2,17 @@
 # when working on remote servers, or when my dotfiles are unavailable.
 
 
+# __ () {
+#   # Lists all copier functions in this file
+#   # List copier functions (starting with _), excluding __, and log function and value sorted alphabetically
+#   for fn in $(declare -F | awk '{print $3}' | grep '^_' | grep -v '^__$' | sort); do
+#     # get string to be copied by capturing the command before /usr/bin/pbcopy
+#     body="$(declare -f "$fn" | grep -v '^}' | grep 'pbcopy' | sed -nE "s/.*printf[[:space:]]+['\"]([^'\"]*)['\"].*/\1/p")"
+#     printf "%s:\n\n%s\n\n" "$fn" "${body:-<could not determine value>}"
+#   done
+# }
 __ () {
-  # Lists all copier functions in this file
-  # List copier functions (starting with _), excluding __, and log function and value sorted alphabetically
-  for fn in $(declare -F | awk '{print $3}' | grep '^_' | grep -v '^__$' | sort); do
-    # get string to be copied by capturing the command before /usr/bin/pbcopy
-    body="$(declare -f "$fn" | grep -v '^}' | grep 'pbcopy' | sed -nE "s/.*printf[[:space:]]+['\"]([^'\"]*)['\"].*/\1/p")"
-    printf "%s:\n\n%s\n\n" "$fn" "${body:-<could not determine value>}"
-  done
+  echo hithereeeee
 }
 
 _rs () { printf "Hill world" | /usr/bin/pbcopy ;}
