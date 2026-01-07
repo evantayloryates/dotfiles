c368e64f12edbd1871378d41adac10b28d742daf
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-07 18:57:11 -0500

    01/07 6:57.11PM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -3,17 +3,17 @@
 # This file must follow the pattern always. no divergences
 # A "Note" is anything that will provide context for the pattern.
 # : is offical syntax. 
-abs     () { realpath "$@"                                                ;} # 
-c       () { cursor "$@"                                                  ;} # 
-o       () { open "$(pwd -P 2>/dev/null || pwd)"                          ;} # 
-clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy             ;} # 
-convert () { magick "$@"                                                  ;} # 
-env     () { /usr/bin/env | sort                                          ;} # 
-ex      () { exiftool "$@"                                                ;} # Note: this will overwrite the /usr/bin/ex command
-ls      () { /bin/ls -AGhlo "$@"                                          ;} # 
-path    () { python3 "$DOTFILES_DIR/src/python/path.py"                   ;} # 
-pip3    () { pip "$@"                                                     ;} # 
-python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"             ;} # 
-python3 () { python "$@"                                                  ;} # 
-src     () { clear; exec "$SHELL" -l                                      ;} # 
+abs     () { realpath "$@"                                                           ;} # 
+c       () { cursor "$@"                                                             ;} # 
+o       () { open "$(pwd -P 2>/dev/null || pwd)"                                     ;} # 
+clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy                        ;} # 
+convert () { magick "$@"                                                             ;} # 
+env     () { /usr/bin/env | sort                                                     ;} # 
+ex      () { exiftool "$@"                                                           ;} # Note: this will overwrite the /usr/bin/ex command
+ls      () { /bin/ls -AGhlo "$@"                                                     ;} # 
+path    () { python3 "$DOTFILES_DIR/src/python/path.py"                              ;} # 
+pip3    () { pip "$@"                                                                ;} # 
+python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"                        ;} # 
+python3 () { python "$@"                                                             ;} # 
+src     () { clear; exec "$SHELL" -l                                                 ;} # 
 reload  () { echo "NO EFFECT\nPlease use "$'\033[35m'"\`src\`"$'\033[0m'" instead.\n"; }
