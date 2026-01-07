c2f74bbdd67da331f1628a9d7e0e008e172c5095
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-07 18:45:58 -0500

    01/07 6:45.58PM

--- src/functions/copiers.sh
+++ src/functions/copiers.sh
@@ -1,4 +1,10 @@
 # Copiers are quick commands to send predefined strings to the clipboard. Useful
 # when working on remote servers, or when my dotfiles are unavailable.
 
+
+__ () {
+  # Lists all copier functions in this file
+  declare -F | awk '{print $3}' | grep '^_' | grep -v '^__$'
+}
+
 _rs () { printf "Hill world" | /usr/bin/pbcopy ;}
