0c954a17e2ead58f94b8e27ce210856b6f23929a
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-13 17:08:09 -0500

    01/13 5:08.09PM

--- src/functions/copiers.sh
+++ src/functions/copiers.sh
@@ -8,7 +8,7 @@ __ () {
   local faded_blue="\033[2;34m"
   local reset="\033[0m"
   
-  printf "\nCOPIERS\n${bold_blue}_rs${reset}:\n${faded_blue}Hill world${reset}\n\n"
+  printf "\nCOPIERS\n${bold_blue}_rs${reset}:\n${faded_blue}Hill world${reset}\n${bold_blue}_cache${reset}:\n${faded_blue}/Library/Caches/nexrender/versions/vast-enhanced-monolith/AE25/v1.2${reset}\n\n"
 }
 
 _rs () { printf "alias rs='DISABLE_SPRING=1 bin/rspec'" | /usr/bin/pbcopy ;}
