20522a23f788784c588761578930edbecb89e975
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-24 19:41:21 -0500

    01/24 7:41.21PM

--- src/python/pathfuncs.py
+++ src/python/pathfuncs.py
@@ -27,6 +27,7 @@ CONFIG = [
   p('dot',        '~/dotfiles'),
   p('dot-old',    '~/.dotfiles'),
   p('gh',         '~/src/github',                      'cd'),
+  p('mac',        '~/src/My Mac Services',             'cd'),
   p('nex',        '~/src/github/nexrender-scripts',    'ssh', commands={'ssh': '/Users/taylor/src/github/nexrender-scripts/scripts/local/ssh'}),
   p('notes',      '~/Desktop/notes'),
   p('pathfuncs',  '~/dotfiles/src/python/pathfuncs.py','cursor', aliases=['pathfunc', 'pathfns', 'pathfn', 'pathfuns', 'pathfun', 'pthfuncs', 'pthfunc', 'pthfns', 'pthfn', 'pthfuns', 'pthfun', 'pfuncs', 'pfunc', 'pfns', 'pfn', 'pfuns', 'pfun' ]),
