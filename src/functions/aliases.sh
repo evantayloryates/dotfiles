2a7e3e6c17abe979cc91721d7dd04689e8d2647b
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-25 02:05:48 -0500

    01/25 2:05.48AM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -5,16 +5,16 @@
 # : is offical syntax. 
 abs     () { realpath "$@"                                                           ;} # 
 c       () { cursor "$@"                                                             ;} # 
-o       () { open "$(pwd -P 2>/dev/null || pwd)"                                     ;} # 
 clip    () { "$@" | perl -pe 'chomp if eof' | /usr/bin/pbcopy                        ;} # 
 convert () { magick "$@"                                                             ;} # 
 env     () { /usr/bin/env | sort                                                     ;} # 
 ex      () { exiftool "$@"                                                           ;} # Note: this will overwrite the /usr/bin/ex command
 ls      () { /bin/ls -AGhlo "$@"                                                     ;} # 
+o       () { open "$(pwd -P 2>/dev/null || pwd)"                                     ;} # 
 path    () { python3 "$DOTFILES_DIR/src/python/path.py"                              ;} # 
 pip3    () { pip "$@"                                                                ;} # 
 python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"                        ;} # 
 python3 () { python "$@"                                                             ;} # 
+reload  () { echo "NO EFFECT\nPlease use "$'\033[35m'"\`src\`"$'\033[0m'" instead.\n";} #
 src     () { clear; exec "$SHELL" -l                                                 ;} # 
 yab     () { source ~/.yabairc                                                       ;} # 
-reload  () { echo "NO EFFECT\nPlease use "$'\033[35m'"\`src\`"$'\033[0m'" instead.\n";} #
