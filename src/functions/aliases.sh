e214329176b74944bfb0282bce3e54956cf4ddac
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-26 16:28:49 -0500

    01/26 4:28.49PM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -18,3 +18,22 @@ python3 () { python "$@"                                                        
 reload  () { echo "NO EFFECT\nPlease use "$'\033[35m'"\`src\`"$'\033[0m'" instead.\n";} #
 src     () { clear; exec "$SHELL" -l                                                 ;} # 
 yab     () { source ~/.yabairc                                                       ;} # 
+
+
+
+
+# Dotfiles sync
+# source "$HOME/dotfiles/sync_dotfiles.sh"
+# alias sync='sync_dotfiles'
+# Git shortcuts
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gb='git branch --sort=-committerdate'
+alias gp='git push'
+alias gl='git log --oneline'
+alias dc="docker compose"
+# Utilities
+# alias mkdir='mkdir -pv'
+alias password="python3 $DOTFILES_DIR/src/python/password.py"
+alias words="open $DOTFILES_DIR/src/__data/words.txt"
