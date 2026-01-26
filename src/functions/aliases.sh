c2058039a608dbc64fbcc18df0b89c45f59d7e60
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-26 18:05:45 -0500

    01/26 6:05.45PM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -3,24 +3,25 @@
 # This file must follow the pattern always. no divergences
 # A "Note" is anything that will provide context for the pattern.
 # : is offical syntax. 
-abs     () { realpath "$@"                                                                 ;} # 
-c       () { clip "$@"                                                                     ;} # 
-clip    () { { printf '$ %s\n' "$*"; "$@"; } | perl -pe 'chomp if eof' | /usr/bin/pbcopy   ;} # 
-convert () { magick "$@"                                                                   ;} # 
-dc      () { docker compose "$@"                                                           ;} # 
-env     () { /usr/bin/env | sort                                                           ;} # 
-ex      () { exiftool "$@"                                                                 ;} # Note: this will overwrite the /usr/bin/ex command
-ls      () { /bin/ls -AGhlo "$@"                                                           ;} # 
-o       () { open "$(pwd -P 2>/dev/null || pwd)"                                           ;} # 
-path    () { python3 "$DOTFILES_DIR/src/python/path.py"                                    ;} # 
-pip3    () { pip "$@"                                                                      ;} # 
-py      () { python "$@"                                                                   ;} # 
-py3     () { python "$@"                                                                   ;} # 
-python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"                              ;} # 
-python3 () { python "$@"                                                                   ;} # 
-reload  () { echo "NO EFFECT\nPlease use "$'\033[35m'"\`src\`"$'\033[0m'" instead.\n"      ;} #
-src     () { clear; exec "$SHELL" -l                                                       ;} # 
-git     () { if [[ $# -eq 1 && "$1" == "branch" ]]; then gbs; else /usr/bin/git "$@"; fi   ;}
+abs     () { realpath "$@"                                                                                     ;} # 
+c       () { clip "$@"                                                                                         ;} # 
+clip    () { { printf '$ %s\n' "$*"; "$@"; } | perl -pe 'chomp if eof' | /usr/bin/pbcopy                       ;} # 
+convert () { magick "$@"                                                                                       ;} # 
+dc      () { docker compose "$@"                                                                               ;} # 
+env     () { /usr/bin/env | sort                                                                               ;} # 
+ex      () { exiftool "$@"                                                                                     ;} # Note: this will overwrite the /usr/bin/ex command
+ls      () { /bin/ls -AGhlo "$@"                                                                               ;} # 
+o       () { open "$(pwd -P 2>/dev/null || pwd)"                                                               ;} # 
+path    () { python3 "$DOTFILES_DIR/src/python/path.py"                                                        ;} # 
+pip3    () { pip "$@"                                                                                          ;} # 
+py      () { python "$@"                                                                                       ;} # 
+py3     () { python "$@"                                                                                       ;} # 
+python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"                                                  ;} # 
+python3 () { python "$@"                                                                                       ;} # 
+reload  () { echo "NO EFFECT\nPlease use "$'\033[35m'"\`src\`"$'\033[0m'" instead.\n"                          ;} #
+_kitsrc () { /Applications/kitty.app/Contents/MacOS/kitty @ load-config "$HOME/.config/kitty/kitty.conf" ;} #
+src     () { clear; exec "$SHELL" -l                                                                           ;} # 
+git     () { if [[ $# -eq 1 && "$1" == "branch" ]]; then gbs; else /usr/bin/git "$@"; fi                       ;} #
 
 
 # Dotfiles sync
