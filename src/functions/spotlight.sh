1312f4dd3db87a37c058aeba6aecb3db02ca1ef3
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 02:43:18 -0500

    01/27 2:43.18AM

--- src/functions/spotlight.sh
+++ src/functions/spotlight.sh
@@ -7,44 +7,28 @@ spotlight_select_action () {
   local primary=$'\e[35m'
   local secondary=$'\e[95m'
 
-  # BLUE
-  # local primary=$'\e[34m'
-  # local secondary=$'\e[94m'
-
-  # CYAN
-  # local primary=$'\e[36m'
-  # local secondary=$'\e[96m'
-
-  # GREEN
-  # local primary=$'\e[32m'
-  # local secondary=$'\e[92m'
-
-  # RED
-  # local primary=$'\e[31m'
-  # local secondary=$'\e[91m'
-
-  # YELLOW
-  # local primary=$'\e[33m'
-  # local secondary=$'\e[93m'
-
   local pipe=$'\e[90m'
   local reset=$'\e[0m'
 
   echo
-  printf '1) add       %s|%s %sspot%s %sadd%s\n'  "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
-  printf '             %s|%s %sspot%s %sa%s\n'     "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
-  printf '             %s|%s %sspot%s %shush%s\n'  "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
-  printf '             %s|%s %sspot%s %sh%s\n'     "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '1) setup     %s|%s %sspot%s %ssetup%s\n'    "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '             %s|%s %sspot%s %ssuppress%s\n'  "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '             %s|%s %sspot%s %ss%s\n'         "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
 
-  printf '2) clean     %s|%s %sspot%s %sclean%s\n' "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
-  printf '             %s|%s %sspot%s %sc%s\n'     "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '2) add       %s|%s %sspot%s %sadd%s\n'      "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '             %s|%s %sspot%s %sa%s\n'        "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '             %s|%s %sspot%s %shush%s\n'     "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '             %s|%s %sspot%s %sh%s\n'        "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
 
-  printf '3) list      %s|%s %sspot%s %slist%s\n' "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
-  printf '             %s|%s %sspot%s %sls%s\n'    "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
-  printf '             %s|%s %sspot%s %sl%s\n'     "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '3) clean     %s|%s %sspot%s %sclean%s\n'    "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '             %s|%s %sspot%s %sc%s\n'        "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
 
-  printf '4) watch     %s|%s %sspot%s %swatch%s\n' "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
-  printf '             %s|%s %sspot%s %sw%s\n'     "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '4) list      %s|%s %sspot%s %slist%s\n'     "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '             %s|%s %sspot%s %sls%s\n'       "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '             %s|%s %sspot%s %sl%s\n'        "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+
+  printf '5) watch     %s|%s %sspot%s %swatch%s\n'    "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
+  printf '             %s|%s %sspot%s %sw%s\n'        "$pipe" "$reset" "$primary" "$reset" "$secondary" "$reset"
 
   echo
   printf 'Selection: '
@@ -53,14 +37,16 @@ spotlight_select_action () {
   echo
 
   case "$choice" in
-    1) spotlight_add_exclusions ;;
-    2) spotlight_clean_exclusions ;;
-    3) spotlight_list_exclusions ;;
-    4) spotlight_watch_exclusions ;;
+    1) spotlight_setup_index_suppression ;;
+    2) spotlight_add_exclusions ;;
+    3) spotlight_clean_exclusions ;;
+    4) spotlight_list_exclusions ;;
+    5) spotlight_watch_exclusions ;;
     *) return 0 ;;
   esac
 
 }
+
 
 spotlight_list_exclusions () {
   local magenta=$'\e[35m'
