b84e5ba8f5fdda2764227bdf7f9b3d989387dcef
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 03:37:27 -0500

    01/27 3:37.27AM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -11,7 +11,11 @@ convert () { magick "$@"                                                        
 dc      () { docker compose "$@"                                                                          ;} # 
 env     () { /usr/bin/env | sort                                                                          ;} # 
 ex      () { exiftool "$@"                                                                                ;} # Note: this will overwrite the /usr/bin/ex command
+ga      () { git add "$@"                                                                                 ;} # 
+gc      () { git commit "$@"                                                                              ;} # 
 git     () { if [[ $# -eq 1 && "$1" == "branch" ]]; then gbs; else /usr/bin/git "$@"; fi                  ;} #
+gl      () { git log --oneline "$@"                                                                       ;} # 
+gp      () { git push "$@"                                                                                ;} # 
 ls      () { /bin/ls -AGhlo "$@"                                                                          ;} # 
 mkdir   () { [ "$#" -eq 1 ] && /bin/mkdir -pv "$1" || /bin/mkdir "$@"                                     ;} #
 o       () { if [ $# -eq 0 ]; then open "$(pwd -P 2>/dev/null || pwd)"; else open "$@"; fi                ;} # 
@@ -23,16 +27,7 @@ python  () { /Users/taylor/.venvs/dotfiles/bin/python -q "$@"                   
 python3 () { python "$@"                                                                                  ;} # 
 reload  () { echo "NO EFFECT\nPlease use "$'\033[35m'"\`src\`"$'\033[0m'" instead.\n"                     ;} #
 src     () { _kitsrc; clear; source "$HOME/dotfiles/src/index.sh"                                         ;} # 
-ga      () { git add "$@"                                                                                 ;} # 
-gc      () { git commit "$@"                                                                              ;} # 
-gp      () { git push "$@"                                                                                ;} # 
-gl      () { git log --oneline "$@"                                                                       ;} # 
-# Git shortcuts
-# alias gs='git status'
-# alias ga='git add'
-# alias gc='git commit'
-# alias gp='git push'
-# alias gl='git log --oneline'
+
 
 alias password="python3 $DOTFILES_DIR/src/python/password.py"
 alias words="open $DOTFILES_DIR/src/__data/words.txt"
