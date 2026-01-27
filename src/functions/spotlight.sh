51cf333e9edf1eb2efca06e7b59e7b89e174de05
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 00:24:39 -0500

    01/27 12:24.39AM

--- src/functions/spotlight.sh
+++ src/functions/spotlight.sh
@@ -30,19 +30,19 @@ spotlight_select_action () {
 
   echo
   printf '1) list     | %sspot%s %slist%s\n' "$primary" "$reset" "$secondary" "$reset"
-  printf '            | %sspot%s %sls%s\n'   "$primary" "$reset" "$secondary" "$reset"
-  printf '            | %sspot%s %sl%s\n'    "$primary" "$reset" "$secondary" "$reset"
+  printf '              %sspot%s %sls%s\n'   "$primary" "$reset" "$secondary" "$reset"
+  printf '              %sspot%s %sl%s\n'    "$primary" "$reset" "$secondary" "$reset"
 
   printf '2) clean    | %sspot%s %sclean%s\n' "$primary" "$reset" "$secondary" "$reset"
-  printf '            | %sspot%s %sc%s\n'     "$primary" "$reset" "$secondary" "$reset"
+  printf '              %sspot%s %sc%s\n'     "$primary" "$reset" "$secondary" "$reset"
 
   printf '3) add      | %sspot%s %sadd%s\n'  "$primary" "$reset" "$secondary" "$reset"
-  printf '            | %sspot%s %sa%s\n'    "$primary" "$reset" "$secondary" "$reset"
-  printf '            | %sspot%s %shush%s\n' "$primary" "$reset" "$secondary" "$reset"
-  printf '            | %sspot%s %sh%s\n'    "$primary" "$reset" "$secondary" "$reset"
+  printf '              %sspot%s %sa%s\n'    "$primary" "$reset" "$secondary" "$reset"
+  printf '              %sspot%s %shush%s\n' "$primary" "$reset" "$secondary" "$reset"
+  printf '              %sspot%s %sh%s\n'    "$primary" "$reset" "$secondary" "$reset"
 
   printf '4) watch    | %sspot%s %swatch%s\n' "$primary" "$reset" "$secondary" "$reset"
-  printf '            | %sspot%s %sw%s\n'     "$primary" "$reset" "$secondary" "$reset"
+  printf '              %sspot%s %sw%s\n'     "$primary" "$reset" "$secondary" "$reset"
 
   echo
   printf 'Selection: '
