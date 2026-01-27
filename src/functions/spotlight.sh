3960a9b5c6638d9ed3dcedfbc80ae4a47bf4a1b6
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-26 23:48:03 -0500

    01/26 11:48.03PM

--- src/functions/spotlight.sh
+++ src/functions/spotlight.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 list_spotlight() {
   sudo /usr/libexec/PlistBuddy -c "Print :Exclusions" /System/Volumes/Data/.Spotlight-V100/VolumeConfiguration.plist 2>/dev/null \
     | grep "^    " \
