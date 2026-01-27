c6a269a062e317ad4011f07d0c08a4314da29f59
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 17:30:51 -0500

    01/27 5:30.51PM

--- src/python/selector.py
+++ src/python/selector.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
+import sys
+
+_print = print
+
+def print(*args, **kwargs):
+  _print(*args, file=sys.stderr, **kwargs)
+
+def send(value):
+  _print(value)
 
 def main():
-  print('app')
+  print('selecting container...')
+  send('app')
 
 if __name__ == '__main__':
   main()
