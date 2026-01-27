d5d98aed71d02fe42b5ef0795cd041e95ba07adf
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 14:44:51 -0500

    01/27 2:44.51PM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -17,7 +17,13 @@ git     () { if [[ $# -eq 1 && "$1" == "branch" ]]; then gbs; else /usr/bin/git 
 gb      () { git branch "$@"                                                                              ;} #
 gl      () { git log --oneline "$@"                                                                       ;} # 
 gp      () { git push "$@"                                                                                ;} # 
-# ls      () { /bin/ls -AGhlo "$@"                                                                         ;} # 
+ls() {
+  {
+    /bin/ls -AGhlo1d "$@" */ 2>/dev/null
+    /bin/ls -AGhlo1p "$@" 2>/dev/null | awk '!/\/$/'
+  }
+}
+
 ls      () { { /bin/ls -AGhlo1d "$@" */ 2>/dev/null; /bin/ls -AGhlo1 "$@" 2>/dev/null | awk '!/\/$/' ; }          ;} # 
 mkdir   () { [ "$#" -eq 1 ] && /bin/mkdir -pv "$1" || /bin/mkdir "$@"                                     ;} #
 o       () { if [ $# -eq 0 ]; then open "$(pwd -P 2>/dev/null || pwd)"; else open "$@"; fi                ;} # 
