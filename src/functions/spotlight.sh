6c5d3d004160f4d599ece95ef26c42f033dc033c
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-26 23:39:28 -0500

    01/26 11:39.28PM

--- src/functions/spotlight.sh
+++ src/functions/spotlight.sh
@@ -4,3 +4,17 @@ list_spotlight() {
     | sed 's/^    //' \
     | sort
 }
+
+
+spotlight_select_action () {
+  echo "spotlight_select_action"
+}
+spotlight_list_exclusions () {
+  echo "spotlight_list_exclusions"
+}
+spotlight_clean_exclusions () {
+  echo "spotlight_clean_exclusions"
+}
+spotlight_add_exclusions () {
+  echo "spotlight_add_exclusions"
+}
