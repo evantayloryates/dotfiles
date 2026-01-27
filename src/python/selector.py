cef5736a2abfcb1719dff6bf34a5a26bc7c610e7
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 17:39:44 -0500

    01/27 5:39.44PM

--- src/python/selector.py
+++ src/python/selector.py
@@ -5,7 +5,7 @@ import os
 _print = print
 COLORS = { 'red': '\033[31m', 'green': '\033[32m', 'yellow': '\033[33m', 'blue': '\033[34m', 'magenta': '\033[35m', 'cyan': '\033[36m', 'white': '\033[37m', 'reset': '\033[0m' }
 
-OPTIONS = OPTIONS = [
+OPTIONS = [
   { 'name': 'app',                'aliases': ['a']     },
   { 'name': 'browser',            'aliases': ['br']    },
   { 'name': 'browserless',        'aliases': ['bl']    },
