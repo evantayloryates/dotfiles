905ee7cf7c6f7f361b9d7c91a2ae2098cefb47bb
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 18:34:11 -0500

    01/27 6:34.10PM

--- src/python/pathfuncs.py
+++ src/python/pathfuncs.py
@@ -18,7 +18,7 @@ CONFIG = [
   p('amp',        '~/src/github/amplify', aliases=['amplify'], commands={
     'disable': 'safemv <path>/.git/hooks/pre-commit <path>/.git/hooks/pre-commit.disabled && echo "pre-commit disabled" || echo "failed to disable"',
     'enable': 'safemv <path>/.git/hooks/pre-commit.disabled <path>/.git/hooks/pre-commit && echo "pre-commit enabled" || echo "failed to enable"',
-    'exec': "_exec_amplify",
+    'exec': "_exec_amplify <args>",
     'ssh': "_ssh_prod",
     'prod': "_ssh_prod",
     'stage': "_ssh_stage",
