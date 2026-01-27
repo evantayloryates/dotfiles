7f109c3c81786f10c85b977fc443ae482347e7c1
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 17:35:32 -0500

    01/27 5:35.31PM

--- src/python/selector.py
+++ src/python/selector.py
@@ -3,11 +3,21 @@ import sys
 import os
 
 _print = print
+COLORS = {
+  'red': '\033[31m',
+  'green': '\033[32m',
+  'yellow': '\033[33m',
+  'blue': '\033[34m',
+  'magenta': '\033[35m',
+  'cyan': '\033[36m',
+  'white': '\033[37m',
+}
 
 SCRIPT_NAME = os.path.basename(__file__)
+LOG_COLOR = COLORS['green']
 
 def print(*args, **kwargs):
-  prefix = f'[{SCRIPT_NAME}]'
+  prefix = f'{LOG_COLOR}[{SCRIPT_NAME}]{COLORS['reset']}'
   _print(prefix, *args, file=sys.stderr, **kwargs)
 
 
