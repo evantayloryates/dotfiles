f7d2f14981799b5b6f35b153bb679c3ac0b057f2
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 22:59:25 -0500

    01/27 10:59.25PM

--- src/python/ls_copiers.py
+++ src/python/ls_copiers.py
@@ -2,8 +2,8 @@ import sys
 
 def main():
   if len(sys.argv) > 1:
-    value = sys.argv[1]
-    sys.stdout.write(f"{value}\n")
+    copiers_path = sys.argv[1]
+    sys.stdout.write(f"{copiers_path}\n")
 
 if __name__ == '__main__':
   main()
