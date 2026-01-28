4ee19fe8fbbaf5cf05c56826b3cc0b77c3eb622d
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 23:13:27 -0500

    01/27 11:13.27PM

--- src/python/ls_copiers.py
+++ src/python/ls_copiers.py
@@ -3,16 +3,22 @@ import sys
 
 COPIER_RE = re.compile(r'^(_[a-z0-9][a-z0-9_-]*) {0,5}\( {0,5}\) {0,5}\{')
 
-def main():
-  copiers_path = sys.argv[1]
-
+def extract_copier_fns(copiers_path):
   with open(copiers_path, 'r') as f:
     lines = f.read().splitlines()
 
+  copier_fns = []
   for line in lines:
     m = COPIER_RE.match(line)
     if m:
       sys.stdout.write(f'{m.group(1)}\n')
+
+def main():
+  copiers_path = sys.argv[1]
+
+  copier_fns = extract_copier_fns(copiers_path)
+  sys.stdout.write(f'{copier_fns}\n')
+  
 
 if __name__ == '__main__':
   main()
