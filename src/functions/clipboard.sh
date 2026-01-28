afe01c503d29076017b797d87cb32448ef5c82c8
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 22:38:31 -0500

    01/27 10:38.31PM

--- src/functions/clipboard.sh
+++ src/functions/clipboard.sh
@@ -10,7 +10,7 @@ clip () {
   } | strip_ansi | /usr/bin/pbcopy
 
   # informational log (not copied to clipboard)
-  printf 'Tip: use \033[35m[%s] cl\033[0m for a more robust copy\n' "$cmd" >&2
+  printf 'Tip: use \033[35m%s cl\033[0m for a more robust copy\n' "$cmd" >&2
 }
 
 setopt extendedglob
