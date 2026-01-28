e601741e56927811a844202c89281361b6057d27
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-28 00:05:56 -0500

    01/28 12:05.56AM

--- src/python/ls_copiers.py
+++ src/python/ls_copiers.py
@@ -87,6 +87,7 @@ def main():
             for variant in variants:
                 result = eval_copier_fn(copiers_path, fn_name, variant)
                 products.append((fn_name, variant, result))
+    
     sorted_products = sorted(products, key=lambda x: x[0])
     for fn_name, variant, result in sorted_products:
         sys.stdout.write(f"{fn_name} {variant}\n")
