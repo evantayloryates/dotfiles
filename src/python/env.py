973b9730ae9ce50c1980eff4210794f0184af7de
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-28 01:54:10 -0500

    01/28 1:54.10AM

--- src/python/env.py
+++ src/python/env.py
@@ -11,7 +11,7 @@ def c(s, color='white'):
 
 NAME_COLOR = 'white'
 # NAME_COLOR = 'magenta_bold_bright'
-DELIMITER_COLOR = 'yellow'
+DELIMITER_COLOR = 'yellow_bright'
 VALUE_COLOR = 'magenta_bold_bright'
 DELIMITER = '='
 items = [
