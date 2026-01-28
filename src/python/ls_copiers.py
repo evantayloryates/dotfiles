c50c0368391e4f7f22a1fc2a880cfc0ef6446e84
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-28 00:25:33 -0500

    01/28 12:25.33AM

--- src/python/ls_copiers.py
+++ src/python/ls_copiers.py
@@ -75,13 +75,18 @@ def display_name(fn_name, variant):
 
 
 COLOR_CODES = {
-    'blue': '34',
+    'black': '30',
+    'red': '31',
     'green': '32',
     'yellow': '33',
+    'blue': '34',
     'magenta': '35',
     'cyan': '36',
     'white': '37',
+    'gray': '90',
+    'dark_blue': '94',
 }
+
 
 
 def c(s, color='white'):
