34654430c96def201a6c332147c3ce9d1d57fec0
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 20:31:13 -0500

    01/27 8:31.13PM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -34,12 +34,13 @@ alias password="python3 $DOTFILES_DIR/src/python/password.py"
 alias words="open $DOTFILES_DIR/src/__data/words.txt"
 
 
+setopt extendedglob
+__CLIP_LASTLINE=''
+preexec() { __CLIP_LASTLINE="$2" }
+
 __clip () {
   local cmd="$__CLIP_LASTLINE"
-  cmd="${cmd%%[[:space:]]#c([[:space:]]#|)}"   # no-op if no match
-  cmd="${cmd%[[:space:]]#c}"                  # remove trailing " c"
-  cmd="${cmd%[[:space:]]#|[[:space:]]#c}"     # remove trailing " | c"
-
+  cmd="${cmd%%[[:space:]]##c([[:space:]]##)#}"  # drop trailing " c"
   {
     printf '$ %s\n' "$cmd"
     cat
