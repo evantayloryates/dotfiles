ea550ac335878f7061dd08b1a94e9e3da3462dee
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 22:58:50 -0500

    01/27 10:58.50PM

--- src/python/ls_copiers.py
+++ src/python/ls_copiers.py
@@ -1,8 +1,9 @@
 import sys
 
 def main():
-  data = sys.stdin.read()
-  sys.stdout.write("data\n")
+  if len(sys.argv) > 1:
+    value = sys.argv[1]
+    sys.stdout.write(value)
 
 if __name__ == '__main__':
   main()
