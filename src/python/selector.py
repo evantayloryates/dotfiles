c2b01b0b559d14f89bfe4a73a9e941b5e8a8003b
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 19:21:40 -0500

    01/27 7:21.40PM

--- src/python/selector.py
+++ src/python/selector.py
@@ -11,7 +11,7 @@ COLORS = {'red': '\033[31m', 'green': '\033[32m', 'yellow': '\033[33m', 'blue': 
 SCRIPT_NAME = os.path.basename(__file__)
 LOG_COLOR = COLORS['green']
 
-# wrapper for print. appends "[<SCRIPTNAME>] " to the beginning of the message
+# Used for debugging. Keep this even if its unused
 def log(*args, **kwargs):
   kwargs.setdefault('end', '\n')
   kwargs.setdefault('flush', False)
