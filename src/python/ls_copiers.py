8c3c6f400fd386cf57f9bbfb9cc9453f41bc23f0
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 23:59:06 -0500

    01/27 11:59.06PM

--- src/python/ls_copiers.py
+++ src/python/ls_copiers.py
@@ -5,6 +5,7 @@ import sys
 COPIER_RE = re.compile(
     r'^(_[a-z0-9](?:[a-z0-9]*(_[a-z0-9]+)*)?) {0,5}\( {0,5}\) {0,5}\{')
 
+
 def extract_variants(fn_name, lines, copiers_path):
     # first, check if any of the lines start with the fn_name__variants function
     full_variants_fn = f"{fn_name}__variants"
@@ -12,16 +13,17 @@ def extract_variants(fn_name, lines, copiers_path):
     for line in lines:
         if line.startswith(full_variants_fn):
             variants_exist = True
-    
+
     if not variants_exist:
         return []
-    variants = eval_command(f'source {sh_quote(copiers_path)}; {sh_quote(full_variants_fn)}')
+    variants = eval_command(
+        f'source {sh_quote(copiers_path)}; {sh_quote(full_variants_fn)}')
     # write to stdout: variants
     sys.stdout.write(f"{variants}\n")
     # then, extract the variants from the result
     return variants.split(' ')
-  
-  
+
+
 def extract_copiers(copiers_path):
     with open(copiers_path, 'r') as f:
         lines = f.read().splitlines()
@@ -73,14 +75,14 @@ def main():
         fn_name = copier['fn']
         variants = copier['variants']
         if len(variants) == 0:
-          result = eval_copier_fn(copiers_path, fn_name)
-          sys.stdout.write(f"{fn_name}\n")
-          sys.stdout.write(f"{result}\n")
-        else:
-          for variant in variants:
-            result = eval_copier_fn(copiers_path, fn_name, variant)
-            sys.stdout.write(f"{fn_name} {variant}\n")
+            result = eval_copier_fn(copiers_path, fn_name)
+            sys.stdout.write(f"{fn_name}\n")
             sys.stdout.write(f"{result}\n")
+        else:
+            for variant in variants:
+                result = eval_copier_fn(copiers_path, fn_name, variant)
+                sys.stdout.write(f"{fn_name} {variant}\n")
+                sys.stdout.write(f"{result}\n")
 
 
 if __name__ == '__main__':
