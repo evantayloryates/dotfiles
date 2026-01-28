7600e698032ffe18d1a4f5c49be12cd78f67e7b7
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 20:33:08 -0500

    01/27 8:33.08PM

--- src/functions/aliases.sh
+++ src/functions/aliases.sh
@@ -32,21 +32,3 @@ src     () { _kitsrc; clear; source "$HOME/dotfiles/src/index.sh"               
 
 alias password="python3 $DOTFILES_DIR/src/python/password.py"
 alias words="open $DOTFILES_DIR/src/__data/words.txt"
-
-
-setopt extendedglob
-__CLIP_LASTLINE=''
-preexec() { __CLIP_LASTLINE="$2" }
-
-__clip () {
-  local cmd="$__CLIP_LASTLINE"
-  cmd="${cmd%%[[:space:]]##\|[[:space:]]##__clip([[:space:]]##)#}" # drop "| __clip"
-  cmd="${cmd%%[[:space:]]##c([[:space:]]##)#}"                    # drop trailing " c"
-  {
-    printf '$ %s\n' "$cmd"
-    cat
-  } | strip_ansi | /usr/bin/pbcopy
-}
-
-alias -g c='| __clip'
-alias -g copy='| __clip'
