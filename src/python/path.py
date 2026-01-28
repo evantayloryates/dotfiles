7b5bc1ed25ebf10786ce8836c1908e5458c5fc71
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-28 01:23:25 -0500

    01/28 1:23.25AM

--- src/python/path.py
+++ src/python/path.py
@@ -122,8 +122,8 @@ normalized.sort(key=lambda p: (segment_count(p), p))
 
 print()
 
-# override_1 = 'black_bold_bright'
-override_1 = random_color(['white', 'black'])
+override_1 = 'white_bold_bright'
+# override_1 = random_color(['white', 'black'])
 override_2 = override_1
 # slash_color = 'white'
 # slash_color = random_color()
