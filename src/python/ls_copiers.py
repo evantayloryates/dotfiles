00e750c4f127ec2073966f5a522819bc351b043f
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 23:00:21 -0500

    01/27 11:00.21PM

--- src/python/ls_copiers.py
+++ src/python/ls_copiers.py
@@ -3,7 +3,13 @@ import sys
 def main():
   if len(sys.argv) > 1:
     copiers_path = sys.argv[1]
-    sys.stdout.write(f"{copiers_path}\n")
+
+    with open(copiers_path, 'r') as f:
+      lines = f.read().splitlines()
+
+    # lines is now an array of strings
+    for line in lines:
+      sys.stdout.write(f'{line}\n')
 
 if __name__ == '__main__':
   main()
