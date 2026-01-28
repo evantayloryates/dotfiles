5e90ec0054d15b070e61160c37b5e449e5cfa9bb
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-27 23:03:00 -0500

    01/27 11:03.00PM

--- src/python/ls_copiers.py
+++ src/python/ls_copiers.py
@@ -1,15 +1,18 @@
+import re
 import sys
 
+COPIER_RE = re.compile(r'^_[a-z][a-z0-9_]*\s*\(\)')
+
 def main():
-  if len(sys.argv) > 1:
-    copiers_path = sys.argv[1]
+  copiers_path = sys.argv[1]
 
-    with open(copiers_path, 'r') as f:
-      lines = f.read().splitlines()
+  with open(copiers_path, 'r') as f:
+    lines = f.read().splitlines()
 
-    # lines is now an array of strings
-    for line in lines:
-      sys.stdout.write(f'{line}\n')
+  copier_lines = [line for line in lines if COPIER_RE.match(line)]
+
+  for line in copier_lines:
+    sys.stdout.write(f'{line}\n')
 
 if __name__ == '__main__':
   main()
