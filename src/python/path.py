80467020111d5c1810779b917538bba72fdce8f3
Evan Taylor Yates <evantayloryates@gmail.com>
2026-01-28 01:31:03 -0500

    01/28 1:31.03AM

--- src/python/path.py
+++ src/python/path.py
@@ -127,9 +127,8 @@ print()
 #  - black_bold_bright
 # override_1 = random_color(['black'])
 override_1 = 'black_bold_bright'
-override_2 = 'yellow_bright'
-# slash_color = random_color(['bright'])
-slash_color_1 = 'black_bold_bright'
+slash_color_1 = 'yellow_bright'
+override_2 = 'black_bold_bright'
 slash_color_2 = 'yellow_bright'
 # for idx, i in enumerate(normalized[:5]):
 print(f"PRIMARY:   {override_1}")
