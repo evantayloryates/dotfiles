0eeb20a7c0576d5932eb3ef2fd413c2f3b19c4e9
Evan Taylor Yates <evantayloryates@gmail.com>
2026-03-25 18:35:49 -0400

    03/25 5:35.49PM

--- src/functions/common.sh
+++ src/functions/common.sh
@@ -117,9 +117,8 @@ function _amplify_update() {
     __log "$(_red "_amplify_update: cannot determine current branch")"
     return 1
   }
-  printf 'dbg: %s=%s\n' "branch" "$(printf %q <<< "branch")"
-  
-  
+  printf 'dbg: %s=%s\n' "branch" "$(printf '%q' "branch")"
+
   case "$branch" in
     master | production)
       __log "$(_red "_amplify_update: refused on branch '${branch}'")"
